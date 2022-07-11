a1abb53951aefef6e8f13388a054f166edf2cae5
Brandon Phillips <brandont.dev@protonmail.com>
2022-07-11 13:44:17 -0600

    Removed secret key again from `settings.py` (oops)

--- brandont_dev/settings.py
+++ brandont_dev/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yo89=flq%bf&fiq=#$dt*b(x6ef@%b1&s_!6+35amy)!$-6^%x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
