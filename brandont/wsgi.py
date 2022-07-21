c00e75bb16d970689cbb380562977f7b67c6b190
Brandon Phillips <brandont.dev@protonmail.com>
2022-07-20 18:32:37 -0600

    Edit `wsgi.py` with new project dir name

--- brandont/wsgi.py
+++ brandont/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brandont_dev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brandont.settings')
 
 application = get_wsgi_application()
