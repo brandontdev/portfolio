caa3712f4e25686fd2675181db49a0148d46207c
Brandon Phillips <brandont.dev@protonmail.com>
2022-07-20 18:51:03 -0600

    `settings.py` imports

--- brandont/settings.py
+++ brandont/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-import sys
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
